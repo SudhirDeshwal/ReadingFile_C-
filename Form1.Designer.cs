d50cab162850ac43ed9db6e3d93befd74e9cdace
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-07-21 04:44:20 -0400

    form added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,16 +31,18 @@
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.listView = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listView
             // 
+            this.listView.BackColor = System.Drawing.Color.Yellow;
             this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1});
             this.listView.HideSelection = false;
-            this.listView.Location = new System.Drawing.Point(330, 83);
+            this.listView.Location = new System.Drawing.Point(330, 33);
             this.listView.Name = "listView";
-            this.listView.Size = new System.Drawing.Size(154, 226);
+            this.listView.Size = new System.Drawing.Size(177, 374);
             this.listView.TabIndex = 0;
             this.listView.UseCompatibleStateImageBehavior = false;
             this.listView.View = System.Windows.Forms.View.Details;
@@ -50,16 +52,28 @@
             this.columnHeader1.Text = "My Friends Name";
             this.columnHeader1.Width = 150;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "The names in list are coming from txt file.";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSlateBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.listView);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,6 +82,7 @@
         private System.Windows.Forms.ColorDialog colorDialog1;
         private System.Windows.Forms.ListView listView;
         private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
